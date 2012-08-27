f861eaabf11d8b60815a7eae4f45d890e3d1e8d8
The0Turtle@gmail.com <The0Turtle@gmail.com@0c779ed9-90c4-d622-26bf-cdbf7f4db2f2>
2012-08-27 20:06:49 +0000

    Mouse dragging draws squares

--- trunk/JamIt/src/gui/RoomLayout.java
+++ trunk/JamIt/src/gui/RoomLayout.java
@@ -1,8 +1,11 @@
+package gui;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -33,18 +36,57 @@ class LayoutPanel extends JPanel {
 	private int xSize = 1280;
 	private int ySize = 800;
 	
-	bool room[][] = new bool[32][32];
+	boolean room[][] = new boolean[32][32];
 	int mouseX = 0;
 	int mouseY = 0;
+	int mouseX2 = 0;
+	int mouseY2 = 0;
 	
 	public LayoutPanel() {
 		addMouseListener(new MouseAdapter() {
-			void mouseClicked (MouseEvent e) {
-				mouseX = (e.getX() - 50)/15;
-				mouseY = (e.getY() - 50)/15;
+			public void mousePressed (MouseEvent e) { // Get the first corner for a selection
+				mouseX = Math.round((e.getX() - 50)/15f);
+				mouseY = Math.round((e.getY() - 50)/15f);
+				
+				if (mouseX < 0) mouseX = 0;
+				if (mouseY < 0) mouseY = 0;
+				if (mouseX > 32) mouseX = 32;
+				if (mouseY > 32) mouseY = 32;
+				mouseX2 = mouseX;
+				mouseY2 = mouseY;
+				repaint();
+			}
+			
+			public void mouseReleased(MouseEvent e) {
+				if (mouseX == mouseX2 || mouseY == mouseY2)
+					return;
+				int top, bottom, left, right;
+				if (mouseX < mouseX2) {
+					left = mouseX;
+					right = mouseX2;
+				} else {
+					left = mouseX2;
+					right = mouseX;
+				}
+				if (mouseY < mouseY2) {
+					top = mouseY;
+					bottom = mouseY;
+				}
 			}
 		});
-		addMouseMotionListener(new MouseAdapter() {});
+		
+		addMouseMotionListener(new MouseAdapter() { // Get the second corner of the selection
+			public void mouseDragged (MouseEvent e) {
+				mouseX2 = Math.round((e.getX() - 50)/15f);
+				mouseY2 = Math.round((e.getY() - 50)/15f);
+				
+				if (mouseX2 < 0) mouseX2 = 0;
+				if (mouseY2 < 0) mouseY2 = 0;
+				if (mouseX2 > 32) mouseX2 = 32;
+				if (mouseY2 > 32) mouseY2 = 32;
+				repaint();
+			}
+		});
 		
 		repaint();
 		
@@ -101,11 +143,25 @@ class LayoutPanel extends JPanel {
 	}
 	
 	public void drawGrid(Graphics b) {
-		for (int x = 0; x < 32; x++) {
-			for (int y = 0; y < 32; y++) {
+		for (int x = 0; x < 33; x++) {
+			for (int y = 0; y < 33; y++) {
 				b.fillOval(50+x*15, 50+y*15, 3, 3);
 			}
 		}
-		b.drawOval(50+mouseX*15, 50+mouseY*15, 4, 4);
+		//b.drawOval(50+mouseX*15-1, 50+mouseY*15-1, 5, 5);
+		//b.drawLine(52+mouseX*15, 52+mouseY*15, 52+mouseX2*15, 52+mouseY2*15);
+		
+		int xcorner;
+		int ycorner;
+		if (mouseX < mouseX2)
+			xcorner = mouseX;
+		else
+			xcorner = mouseX2;
+		if (mouseY < mouseY2)
+			ycorner = mouseY;
+		else
+			ycorner = mouseY2;
+		
+		b.drawRect(52+xcorner*15, 52+ycorner*15, Math.abs((mouseX-mouseX2)*15), Math.abs((mouseY-mouseY2)*15));
 	}
 }
