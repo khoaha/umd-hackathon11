228d124812a9e8cfeff75984269d91623fb87952
brian.gdall@gmail.com <brian.gdall@gmail.com@0c779ed9-90c4-d622-26bf-cdbf7f4db2f2>
2012-08-28 08:24:14 +0000

    commit that maybe works who knows

--- trunk/JamItIn/src/com/example/jamitin/MainActivity.java
+++ trunk/JamItIn/src/com/example/jamitin/MainActivity.java
@@ -4,24 +4,35 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.util.ArrayList;
 
 
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private Button requestButton;
 	private EditText requestName;
 	private Socket sockToSql;
+	private ListView listofcontacts;
 	//private URL serverURL
 	
     @Override
@@ -30,9 +41,19 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         requestButton = (Button) findViewById(R.id.requestButton);
         requestName = (EditText) findViewById(R.id.requestName);
+        listofcontacts = (ListView) findViewById(R.id.listView);
         connectToServer();
+       
+        
     }
-
+    
+    private String getPhoneNumber(){
+    	TelephonyManager tMgr =(TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        
+    	return tMgr.getLine1Number();
+    }
+    
+    
     private void connectToServer() {
 		
 		new ConnectTask().execute();
@@ -52,6 +73,25 @@ public class MainActivity extends Activity {
 		}
     	
     }
+    
+    public void populateListView(ArrayList<String> list){
+    	ArrayAdapter<String> madapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, list);
+    	listofcontacts.setAdapter(madapter);
+    	listofcontacts.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+    		
+    	});
+    }
 
 	public void onResume(){
 		super.onResume();
@@ -63,8 +103,11 @@ public class MainActivity extends Activity {
 			}
     		
     	});
+    	
     }
     
+	
+	
     protected void requestContact(String s) {
 		// TODO Auto-generated method stub
 		
