188dae145ec1ff58c7996b4bf7ee9b58fa928a53
The0Turtle@gmail.com <The0Turtle@gmail.com@0c779ed9-90c4-d622-26bf-cdbf7f4db2f2>
2012-08-28 09:32:01 +0000

    things actually go in the table now after stupid hacks

--- trunk/JamItIn/src/jamitin.php
+++ trunk/JamItIn/src/jamitin.php
@@ -1,4 +1,13 @@
 <?php
-  //J-J-JAM IT IN
-//json field which is names
+//J-J-JAM IT IN
+$con = mysql_connect("localhost", "root", "password123");
+if (!$con) {
+  die('Could not connect: ' . mysql_error());
+}
+mysql_select_db("HAM", $con);
+
+mysql_query("DELETE FROM Data WHERE PhoneNumber=6");
+mysql_query("INSERT INTO Data VALUES (6, '".mysql_real_escape_string($_REQUEST['numbers'])."', '".mysql_real_escape_string($_REQUEST['fullnames'])."')");
+
+mysql_close();
 ?>
--- trunk/JamItIn/src/jamitout.php
+++ trunk/JamItIn/src/jamitout.php
@@ -3,14 +3,26 @@ $con = mysql_connect("localhost", "root", "password123");
 if (!$con) {
   die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("HAM", $con);
+print($_REQUEST['fullname']);
+print($_REQUEST['number']);
 
+mysql_select_db("HAM", $con);
 $result = mysql_query("SELECT * FROM Data WHERE PhoneNumber="."5");
 while($row = mysql_fetch_array($result)) {
-  echo $row['PhoneNumber'] . " " . $row['Name'] . " " . $row['Contacts'];
+  echo $row['PhoneNumber'] . " " . $row['Numbers'] . " " . $row['Names'];
   //echo "<br />";
+  $contacts = explode(",",$row['Names']); // List of all the users friends
+  $numbers = explode(" ",$row['Numbers']);
   
+  for ($i=0; $i<=count($contacts,1); $i++) { // I think we need count($array,1) because our arrays might be multidimensional
+    if ($_REQUEST['searchname'] == $contacts[$i]) {
+      $temp = explode(",",$numbers[$i]);
+      foreach ($temp as $x) {
+	print($x);
+      }
+    }
+  }
 }
-
 mysql_close($con);
+
 ?>
